880ee996f8584e94477a8dfb8fcf4993cae4c3a7
陈钢 <james_gchen@126.com>
2020-06-03 15:47:22 +0800

    if elif练习

--- vulcan.py
+++ vulcan.py
@@ -1,1 +1,12 @@
-print ('你好中国，我是宇宙飞创')
+# 小明身高练习程序
+bmi = 80.5/pow(1.75,2)
+if bmi >32:
+    print ('严重肥胖')
+elif 28 < bmi <=32:
+    print('肥胖')
+elif 25 < bmi <=28:
+    print ('过重')
+elif 18.5 < bmi <=25:
+    print ('正常')
+elif bmi <=18.5:
+    print ('过轻')
