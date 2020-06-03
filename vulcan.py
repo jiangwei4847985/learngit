c8a6f7c1965b30fcb5288f1fc30799634f3b185c
陈钢 <james_gchen@126.com>
2020-06-03 16:29:07 +0800

    循环函数练习

--- vulcan.py
+++ vulcan.py
@@ -1,1 +1,48 @@
-print ('你好中国，我是宇宙飞创')
+## for 循环练习
+
+name = ['陈钢','李青青','李卡卡']
+for name in name:
+    print(name)
+
+sum = 0
+for x in range(101):
+    sum = sum + x
+print (sum)
+
+## 练习2 请利用循环依次对list中的每个名字打印出Hello, xxx!：
+
+name = ['Hello','xxx!']
+for name in name:
+    print (name)
+print ('利用循环依次对list中的每个名字打印出Hello, xxx!')
+
+## break 练习  输出1~100，到58时终止
+
+a = 1
+while a <= 100:
+    if a >58:
+        break
+    print (a)
+    a = a + 1
+print ('END')
+print('输出1~100，到58时终止')
+
+## continue 练习  符合条件时跳过并进入下一个循环 比如 输入输出1~100中的奇数
+
+b=1 
+while b <= 100:
+    b = b + 1
+    if b % 2 ==0:
+        continue
+    print (b)
+print ("END")
+print('输入输出1~100中的奇数')
+
+## 循环练习 ：求100以内奇数的和
+
+c = 1
+for z in range(100):
+    if(z%2)==1:
+        c=c+z
+print (c)
+print ('循环练习，求100以内奇数的和')
